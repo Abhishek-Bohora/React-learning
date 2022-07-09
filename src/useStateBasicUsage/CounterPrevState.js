7e6afea8f7dbfacddc632a7af191d4378dc6a9c6
Abhishek-Bohora <abhishek.bohora16@gmail.com>
2022-07-09 19:12:45 +0545

    CounterPrevState updated1

--- src/useStateBasicUsage/CounterPrevState.js
+++ src/useStateBasicUsage/CounterPrevState.js
@@ -4,15 +4,22 @@ import React, { useState } from 'react'
 export const CounterPrevState = () => {
     const[age, setAge] = useState(42);
 
-    function handleChange(){
+    function ageIncrementer(){
         // changing the value based on the previous state
         setAge(a => a+1);  //this is an updater function which takes the value from previous state and calculate the next state from it
-        setAge(a => a+1);
-        setAge(a => a+1);
     }
   return (
     <>
-        <button onClick={handleChange}>Incrementer</button>
+        <button onClick={()=>{
+            ageIncrementer();
+            ageIncrementer();
+            ageIncrementer();
+        }
+        }>+3</button>
+        <button onClick={()=>{
+            ageIncrementer();
+        }
+        }>+1</button>
         <p>Your age is: {age}</p>
     </>
   )
